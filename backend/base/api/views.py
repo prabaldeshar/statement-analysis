d7f1dd161d8af3712a21514a73c9b7330209096f
Prabal Deshar <prabaldeshar@gmail.com>
2024-04-27 23:04:57 +0545

    add: rest API with authentication

---
+++ backend/base/api/views.py
@@ -0,0 +1,40 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from rest_framework.permissions import IsAuthenticated
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(["GET"])
+def get_routes(request):
+    routes = ["/api/token", "/api/token/refresh"]
+
+    return Response(routes)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_data(request):
+    user = request.user
+    print(user)
+    sample_data = {"test": "test_data"}
+
+    return Response(sample_data)
